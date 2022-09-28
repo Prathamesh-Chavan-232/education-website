6ca2ecfab6e5bf0a5efc3eb6ce94e91e2bfc8a0a
Prathamesh Chavan <prathamesh.chavanpsc2018@gmail.com>
2022-09-29 03:25:10 +0530

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const burger = document.querySelector(".burger");
+const navbar = document.querySelector(".navbar");
+const leftNav = document.querySelector(".left-nav");
+const rightNav = document.querySelector(".right-nav");
 
+burger.addEventListener("click", () => {
+  navbar.classList.toggle("nav-height");
+  leftNav.classList.toggle("hidden");
+  rightNav.classList.toggle("hidden");
+});
